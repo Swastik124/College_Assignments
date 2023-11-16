57c60064d67a1db276f6406c8cd14cc93db11513
Swastik Kumar Mohanty <142680486+Swastik124@users.noreply.github.com>
2023-11-16 21:58:37 +0530

    Add files via upload

--- 2nd Year/JAVA/Assignment2/Q2.java
+++ 2nd Year/JAVA/Assignment2/Q2.java
@@ -3,26 +3,25 @@ import java.util.Scanner;
 public class Q2 {
     public static void main(String[] args)
     {
-        Scanner o=new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         int x,s,y;
         System.out.println("Enter the size of Array: ");
-        s=o.nextInt();
+        s=sc.nextInt();
         int a[]=new int[s];
         System.out.println("Enter elements into the Array: ");
-        for(int i=0;i<a.length;i++){
-            a[i]=o.nextInt();
+        for(int i=0;i<s;i++){
+            a[i]=sc.nextInt();
         }
-        for(int i=0,j=1;i<a.length;j++)
+        for(int i=0;i<s;i++)
         {
-            if(a[i]>a[j])
+            if(a[i]<a[i+1])
             {
-                x=a[j];
-                a[j]=a[i];
-                a[i]=x;
-                i++;
+                x=a[i];
+                a[i]=a[i+1];
+                a[i+1]=x;
             }
         }
-        o.close();
+        sc.close();
     }
         
 }
