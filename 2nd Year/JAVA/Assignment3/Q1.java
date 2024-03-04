fe9aa93f82cc53ce3aa7304bb5e788f6f4d39d88
Swastik Kumar Mohanty <142680486+Swastik124@users.noreply.github.com>
2024-03-04 22:44:19 +0530

    Add files via upload

---
+++ 2nd Year/JAVA/Assignment3/Q1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Sum{
+    public void sum()
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter two numbers");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println("Sum of the two numbers is "+(a+b));
+        sc.close();
+    }
+}
+public class Q1 {
+    public static void main(String[] args) {
+        Sum ob=new Sum();
+        ob.sum();
+    }    
+}
